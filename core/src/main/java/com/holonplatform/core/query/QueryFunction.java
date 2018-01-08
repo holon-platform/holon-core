80fe45a1339a1cd8f4c937b9c3f7103e469afc62
Riccardo Righi <rr9379@gmail.com>
2018-01-08 11:31:09 +0100

    Move all function builders in QueryFunction

--- core/src/main/java/com/holonplatform/core/query/QueryFunction.java
+++ core/src/main/java/com/holonplatform/core/query/QueryFunction.java
@@ -15,6 +15,8 @@
  */
 package com.holonplatform.core.query;
 
+import java.util.Date;
+
 import com.holonplatform.core.Expression;
 import com.holonplatform.core.internal.query.AvgFunction;
 import com.holonplatform.core.internal.query.CountFunction;
@@ -23,6 +25,14 @@ import com.holonplatform.core.internal.query.MinFunction;
 import com.holonplatform.core.internal.query.SumFunction;
 import com.holonplatform.core.query.StringFunction.Lower;
 import com.holonplatform.core.query.StringFunction.Upper;
+import com.holonplatform.core.query.TemporalFunction.CurrentDate;
+import com.holonplatform.core.query.TemporalFunction.CurrentTimestamp;
+import com.holonplatform.core.query.TemporalFunction.Day;
+import com.holonplatform.core.query.TemporalFunction.Hour;
+import com.holonplatform.core.query.TemporalFunction.Minute;
+import com.holonplatform.core.query.TemporalFunction.Month;
+import com.holonplatform.core.query.TemporalFunction.Second;
+import com.holonplatform.core.query.TemporalFunction.Year;
 
 /**
  * Represents a generic <em>function</em> to be used in a {@link Query} definition.
@@ -39,10 +49,58 @@ public interface QueryFunction<T> extends Expression {
 	 */
 	Class<? extends T> getResultType();
 
+	// Aggregate function builders
+
+	/**
+	 * Creates an aggregate function to count query results.
+	 * @return A new {@link Count} function
+	 */
+	public static Count count() {
+		return Count.create();
+	}
+
+	/**
+	 * Creates an aggregate function to calculate the average value of a query result.
+	 * @return A new {@link Avg} function
+	 */
+	public static Avg avg() {
+		return Avg.create();
+	}
+
+	/**
+	 * Creates an aggregate function to get the smallest value of a query result.
+	 * @param <T> Result type
+	 * @param resultType Function and query result type
+	 * @return A new {@link Min} function
+	 */
+	public static <T> Min<T> min(Class<? extends T> resultType) {
+		return Min.create(resultType);
+	}
+
+	/**
+	 * Creates an aggregate function to get the largest value of a query result.
+	 * @param <T> Result type
+	 * @param resultType Function and query result type
+	 * @return A new {@link Max} function
+	 */
+	public static <T> Max<T> max(Class<? extends T> resultType) {
+		return Max.create(resultType);
+	}
+
+	/**
+	 * Creates an aggregate function to sum query results.
+	 * @param <T> Result type
+	 * @param resultType Function and query result type
+	 * @return A new {@link Sum} function
+	 */
+	public static <T> Sum<T> sum(Class<? extends T> resultType) {
+		return Sum.create(resultType);
+	}
+
 	// String function builders
 
 	/**
-	 * Create a function to convert a String data type into lowercase.
+	 * Creates a function to convert a String data type into lowercase.
 	 * @return A new {@link Lower} function
 	 */
 	public static Lower lower() {
@@ -50,11 +108,94 @@ public interface QueryFunction<T> extends Expression {
 	}
 
 	/**
-	 * Create a function to convert a String data type into uppercase.
+	 * Creates a function to convert a String data type into uppercase.
 	 * @return A new {@link Lower} function
 	 */
 	public static Upper upper() {
 		return Upper.create();
+	}
+
+	// Temporal function builders
+
+	/**
+	 * Create a function to obtain the current date as a {@link Date}.
+	 * @return A function to obtain the current date as a {@link Date}.
+	 */
+	public static CurrentDate currentDate() {
+		return CurrentDate.create();
+	}
+
+	/**
+	 * Create a function to obtain the current timestamp as a {@link Long} number which represents the milliseconds
+	 * since January 1, 1970, 00:00:00 GMT (Unix epoch). A negative number is the number of milliseconds before January
+	 * 1, 1970, 00:00:00 GMT.
+	 * @return A function to obtain the current timestamp
+	 */
+	public static CurrentTimestamp currentTimestamp() {
+		return CurrentTimestamp.create();
+	}
+
+	/**
+	 * Create a function to extract the <em>year</em> part of a temporal data type.
+	 * @return A function to extract the <em>year</em> part of a temporal data type.
+	 */
+	public static Year year() {
+		return Year.create();
+	}
+
+	/**
+	 * Create a function to extract the <em>month</em> part of a temporal data type.
+	 * <p>
+	 * The month range index is between 1 and 12.
+	 * </p>
+	 * @return A function to extract the <em>month</em> part of a temporal data type.
+	 */
+	public static Month month() {
+		return Month.create();
+	}
+
+	/**
+	 * Create a function to extract the <em>day</em> part of a temporal data type.
+	 * <p>
+	 * The day is intended as the day of month and the day range index is between 1 and 31.
+	 * </p>
+	 * @return A function to extract the <em>day</em> part of a temporal data type.
+	 */
+	public static Day day() {
+		return Day.create();
+	}
+
+	/**
+	 * Create a function to extract the <em>hour</em> part of a temporal data type.
+	 * <p>
+	 * The 24-hour clock is used and the hour range index is between 0 and 23.
+	 * </p>
+	 * @return A function to extract the <em>hour</em> part of a temporal data type.
+	 */
+	public static Hour hour() {
+		return Hour.create();
+	}
+
+	/**
+	 * Create a function to extract the <em>minute</em> part of a temporal data type.
+	 * <p>
+	 * The minute range index is between 0 and 59.
+	 * </p>
+	 * @return A function to extract the <em>minute</em> part of a temporal data type.
+	 */
+	public static Minute minute() {
+		return Minute.create();
+	}
+
+	/**
+	 * Create a function to extract the <em>second</em> part of a temporal data type.
+	 * <p>
+	 * The second range index is between 0 and 59.
+	 * </p>
+	 * @return A function to extract the <em>second</em> part of a temporal data type.
+	 */
+	public static Second second() {
+		return Second.create();
 	}
 
 	// -------
