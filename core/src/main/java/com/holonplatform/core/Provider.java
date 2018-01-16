1706d5e58d8319abd62de1fb97189e9f777f40f6
Riccardo Righi <rr9379@gmail.com>
2018-01-16 11:56:49 +0100

    New optional object Provider interface

---
+++ core/src/main/java/com/holonplatform/core/Provider.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2016-2017 Axioma srl.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.holonplatform.core;
+
+import java.util.Optional;
+
+/**
+ * Optional object provider.
+ * 
+ * @param <T> Provided object type
+ *
+ * @since 5.1.0
+ */
+@FunctionalInterface
+public interface Provider<T> {
+
+	/**
+	 * Get the object provided by this provider, if available.
+	 * @return The optional provided object
+	 */
+	Optional<T> get();
+
+}
