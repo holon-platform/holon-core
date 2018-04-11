3c324a3a3df63f493dbfb36c82880a69401feb36
Riccardo Righi <rr9379@gmail.com>
2018-04-11 16:18:02 +0200

    5.1.0 documentation updates

--- documentation/src/main/java/com/holonplatform/core/examples/ExampleSpring.java
+++ documentation/src/main/java/com/holonplatform/core/examples/ExampleSpring.java
@@ -15,7 +15,6 @@
  */
 package com.holonplatform.core.examples;
 
-import java.net.URISyntaxException;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,19 +32,20 @@ import com.holonplatform.spring.EnableTenantScope;
 import com.holonplatform.spring.EnvironmentConfigPropertyProvider;
 import com.holonplatform.spring.ScopeTenant;
 import com.holonplatform.spring.SpringRestClient;
+import com.holonplatform.spring.TenantScopeManager;
 
 @SuppressWarnings("unused")
 public class ExampleSpring {
 
-	// tag::context[]
 	class TestResource {
 	}
 
+	// tag::context[]
 	@Configuration
-	@EnableBeanContext
+	@EnableBeanContext // <1>
 	class SpringConfig {
 
-		@Bean(name = "testResource")
+		@Bean(name = "testResource") // <2>
 		public TestResource testResource() {
 			return new TestResource();
 		}
@@ -53,25 +53,41 @@ public class ExampleSpring {
 	}
 
 	public void getContextResource() {
-		// lookup using resurce name which matches the bean name
-		Optional<TestResource> resource = Context.get().resource("testResource", TestResource.class);
+		// lookup by name and type
+		Optional<TestResource> resource = Context.get().resource("testResource", TestResource.class); // <3>
 		// lookup by type
-		resource = Context.get().resource(TestResource.class);
+		resource = Context.get().resource(TestResource.class); // <4>
 	}
 	// end::context[]
 
+	public void env() {
+		// tag::env[]
+		org.springframework.core.env.Environment environment = obtainSpringEnvironment(); // <1>
+
+		// build a ConfigPropertyProvider using Spring Environment as property source
+		ConfigPropertyProvider provider = EnvironmentConfigPropertyProvider.create(environment); // <2>
+
+		String value = provider.getProperty("test.property.name", String.class); // <3>
+		// end::env[]
+	}
+
+	private static Environment obtainSpringEnvironment() {
+		return null;
+	}
+
 	// tag::tenant[]
 	@Configuration
-	@EnableTenantScope
+	@EnableTenantScope // <1>
 	class TenantScopeConfig {
 
 		@Bean
-		public TenantResolver tenantResolver() {
-			return () -> Optional.of("test"); // provide a meaningful current tenant id resolution strategy...
+		public TenantResolver tenantResolver() { // <2>
+			// provide a meaningful current tenant id resolution strategy...
+			return () -> Optional.of("test");
 		}
 
 		@Bean
-		@ScopeTenant
+		@ScopeTenant // <3>
 		public TestResource testResource() {
 			// a different instance of the bean will be provided for each tenant id
 			return new TestResource();
@@ -80,32 +96,44 @@ public class ExampleSpring {
 	}
 	// end::tenant[]
 
-	// tag::env[]
+	// tag::scopemanager[]
 	@Autowired
-	Environment environment;
+	TenantScopeManager tenantScopeManager;
 
-	public void env() {
-		// build a ConfigPropertyProvider using Spring Environment as property source
-		ConfigPropertyProvider provider = EnvironmentConfigPropertyProvider.create(environment);
-
-		String value = provider.getProperty("test.property.name", String.class);
+	void discardTenantScopedBeans() {
+		tenantScopeManager.discardTenantBeanStore("a_tenant_id"); // <1>
 	}
-	// end::env[]
+	// end::scopemanager[]
 
-	// tag::restclient[]
-	@Autowired
-	RestTemplate restTemplate;
+	public void restclient1() {
+		// tag::restclient1[]
+		RestTemplate restTemplate = getRestTemplate(); // <1>
 
-	public void restclient() throws URISyntaxException {
-
-		// Create a SpringRestClient
-		RestClient client = SpringRestClient.create(restTemplate);
-
-		// obtain the RestClient using default creation methods
-		client = RestClient.create();
-
-		client = RestClient.create(SpringRestClient.class.getName());
+		RestClient client = SpringRestClient.create(restTemplate); // <2>
+		// end::restclient1[]
 	}
-	// end::restclient[]
+
+	// tag::restclient2[]
+	@Configuration
+	@EnableBeanContext // <1>
+	class Config {
+
+		@Bean // <2>
+		public RestTemplate restTemplate() {
+			return new RestTemplate();
+		}
+
+	}
+
+	void restclient() {
+		RestClient client = RestClient.create(); // <3>
+
+		client = RestClient.create(SpringRestClient.class.getName()); // <4>
+	}
+	// end::restclient2[]
+
+	private static RestTemplate getRestTemplate() {
+		return null;
+	}
 
 }
